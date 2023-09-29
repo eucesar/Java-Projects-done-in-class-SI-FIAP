4f795e826991277586561e6e01cf55de437d1b15
Cesar Iglesias <cesaribneto.job@gmail.com>
2023-09-28 22:01:42 -0300

    aula 7 + metodo

---
+++ Java - 1°Ano/2° Semestre/metodo/src/metodo/ex1.java
@@ -0,0 +1,16 @@
+package metodo;
+
+public class ex1 {
+
+	public static void main(String[] args) {
+
+		imprimir();
+		
+	}
+
+	public static void imprimir () {
+		
+		System.out.println("hoje é quinta-feira");
+		
+	}
+}
