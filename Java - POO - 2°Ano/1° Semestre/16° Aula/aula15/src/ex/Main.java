f2afdce40112ac241befbc58674a217de16690d5
Cesar Iglesias <cesaribneto.job@gmail.com>
2024-04-24 20:15:28 -0300

    aula16

---
+++ Java - POO - 2°Ano/1° Semestre/16° Aula/aula15/src/ex/Main.java
@@ -0,0 +1,14 @@
+package ex;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		
+		Empregado[] empregado = new Empregado[4]; //4 empregadp
+		
+		empregado[0] = new EmpregadoHorista(null, 0, 0, 0);
+	
+	}
+
+}
